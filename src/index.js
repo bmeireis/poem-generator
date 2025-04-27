26dcbc89dee733c89621b9a2636d9f8077425a1d
bmeireis <barbara.reis2995@gmail.com>
2025-04-27 17:58:25 +0100

    Upgraded the API

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function generatePoem(event) {
   let apiKey = "5878of651546aa8t45eef270aa5abb3b";
   let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
   let context =
-    "You are a poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br/>. Make sure to follow the user instructions.";
+    "You are a poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br/>. Make sure to follow the user instructions. Do not include a title with the poem. Sign the poem with `SheCodes AI` inside a <trong> element at the end of the poem and NOT at the beginning.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiURL).then(displayPoem);
